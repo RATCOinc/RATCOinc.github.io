14de6e991dd2ac6e8f13cddddb8181a8eaf63bf3
RATCOinc <ratcoinc@gmail.com>
2016-08-23 12:59:09 +0530

    VLSI uploads

---
+++ VLSI Placement/sample data/data0/a.pl
@@ -0,0 +1,7 @@
+# UCLA pl 1.0 
+# node_name 	 ll_xcoord 	 ll_ycoord 	 : 	 orientation 	 movetype 
+	o0		50.0		32.0	:	N		
+	o1		45.0		32.0	:	N		
+	o2		20.0		32.0	:	N		
+	o3		31.0		52.0	:	N		/FIXED
+	p0		15.0		47.0	:	N		/FIXED_NI
---
+++ VLSI Placement/sample data/data0/abc.pl
@@ -0,0 +1,7 @@
+# UCLA pl 1.0 
+# node_name 	 ll_xcoord 	 ll_ycoord 	 : 	 orientation 	 movetype 
+	o0		15.0		47.0	:	N		
+	o1		15.0		47.0	:	N		
+	o2		67.0		32.0	:	N		
+	o3		22.0		32.0	:	N		/FIXED
+	p0		20.0		32.0	:	N		/FIXED_NI
